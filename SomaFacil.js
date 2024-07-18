daf12a89cd96f1434ecc450780b0d220d6a772f1
Keven Melo <115496525+Keven-Melo@users.noreply.github.com>
2024-07-17 22:35:35 -0300

    Delete SomaFacil.js

--- SomaFacil.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const input = fs.readFileSync(0, "utf8");
-
-let A = parseInt(input.split("\n")[0])
-let B = parseInt(input.split("\n")[1])
-
-let soma = A + B
-
-console.log(soma)
